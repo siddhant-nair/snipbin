25bf2f585c0269d9a5a33a24896832f6f491fb62
siddhant-nair <siddhn10@gmail.com>
2025-02-04 16:04:16 +0530

    beta of indexing - prefix - and ranking done

--- prefix-search/prefixSearch.go
+++ prefix-search/prefixSearch.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
+	"math"
 	"os"
+	"sort"
 	"strings"
 )
 
@@ -13,6 +16,11 @@ type Snippet struct {
 	Description string //2 point
 	Example     string //1 point
 	Tags        string //1 point
+}
+
+type Tuple[T1 any, T2 any] struct {
+	first  T1
+	second T2
 }
 
 func (s *Snippet) getList() []string {
@@ -25,24 +33,51 @@ func (s *Snippet) getList() []string {
 	}
 }
 
-func preprocess(indexMap *map[string]int, snippet *Snippet) {
+func tokenisationCondition(r rune) bool {
+	return r == '.' || r == ' ' || r == ',' || r == '`' || r == '\n'
+}
+
+func preprocess(snippet *Snippet) map[string]int {
+
+	indexMap := map[string]int{}
 
 	for scoreIndex, v := range snippet.getList() {
 		str := strings.ToLower(v)
-		tokens := strings.FieldsFunc(str, func(r rune) bool {
-			return r == '.' || r == ' ' || r == ',' || r == '`' || r == '\n'
-		})
+		tokens := strings.FieldsFunc(str, tokenisationCondition)
 
 		for _, val := range tokens {
 			for i := 0; i < len(val); i++ {
-				if _, exists := (*indexMap)[val[:i]]; exists {
-					(*indexMap)[val[:i]] += scoreIndex
+				if _, exists := indexMap[val[:i]]; exists {
+					indexMap[val[:i+1]] += int(math.Max(1, float64(4-scoreIndex)))
 				} else {
-					(*indexMap)[val[:i]] = scoreIndex
+					indexMap[val[:i+1]] = int(math.Max(1, float64(4-scoreIndex)))
 				}
 			}
 		}
 	}
+
+	return indexMap
+}
+
+func showScores(searchString string, indexList *[]map[string]int) []Tuple[int, int] {
+
+	// scoreCard := make([]int, len(*indexList))
+	scoreCard := []Tuple[int, int]{}
+
+	for i, _ := range *indexList {
+		scoreCard = append(scoreCard, Tuple[int, int]{i, 0})
+	}
+
+	searchStrTokens := strings.FieldsFunc(searchString, tokenisationCondition)
+
+	for _, token := range searchStrTokens {
+		for i, indexMap := range *indexList {
+			// fmt.Println(indexMap)
+			scoreCard[i].second += indexMap[token]
+		}
+	}
+
+	return scoreCard
 }
 
 func main() {
@@ -62,9 +97,27 @@ func main() {
 		return
 	}
 
-	var indexMap map[string]int
+	indexList := []map[string]int{}
 
-	preprocess(&indexMap, snippetList[0])
+	for _, v := range snippetList {
+		indexList = append(indexList, preprocess(v))
+	}
 
-	// fmt.Println(snippetList[2].Summary)
+	// fmt.Println(indexList)
+	fmt.Println("Enter Search String")
+
+	inputStr := bufio.NewReader(os.Stdin)
+	searchStr, err := inputStr.ReadString('\n')
+
+	scoreList := showScores(searchStr, &indexList)
+
+	sort.Slice(scoreList, func(i, j int) bool {
+		return scoreList[i].second > scoreList[j].second
+	})
+
+	for i := 0; i < 3; i++ {
+		fmt.Println(snippetList[scoreList[i].first])
+		fmt.Println("")
+	}
+	// fmt.Println(scoreList)
 }

7b69bbe65086e0a924ca340d78ee492bb5c4324a
siddhant-nair <siddhn10@gmail.com>
2025-03-06 20:53:33 +0530

    experimenting with slug

--- frontend/snipbin/src/util/slugify.ts
+++ frontend/snipbin/src/util/slugify.ts
@@ -2,7 +2,8 @@ export default function slugify(str: string) : string {
     str = str.replace(/^\s+|\s+$/g, ''); // trim leading/trailing white space
     str = str.toLowerCase(); // convert string to lowercase
     str = str
-      .replace(/[^a-z0-9 -]/g, '') // remove any non-alphanumeric characters
+      // .replace(/[^a-z0-9 -]/g, '') // remove any non-alphanumeric characters
+      .replace(/[^a-z0-9 - +]/g, '') // remove any non-alphanumeric characters
       .replace(/\s+/g, '-') // replace spaces with hyphens
       .replace(/-+/g, '-'); // remove consecutive hyphens
     return str;
